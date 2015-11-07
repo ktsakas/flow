1168cf6be1d0d4d7afdec998441705a088acb2b2
ktsakas <kostastsakas@gmail.com>
2015-11-06 22:46:39 -0500

    Added some mongo queries

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
 var app = express();
+var PORT = 8080;
 
+mongoose.connect('mongodb://localhost');//'localhost:27017');
 var Playlist = mongoose.model('Playlist', {
 	name: String,
 	user: String,
@@ -9,27 +13,38 @@ var Playlist = mongoose.model('Playlist', {
 });
 
 // Get all playlists for user
-app.get('/users/:user_id/playlists', function (req, res) {
-	
+app.get('/users/:userId/playlists', function (req, res) {
+	Playlist.find({
+		user: req.params.userId
+	}, function (err, playlist) {
+		console.log(playlist);
+	});
 });
 
 // Get playlists for user
-app.get('/playlists/:playlist_id', function (req, res) {
+app.get('/playlists/:playlistId', function (req, res) {
 
 });
 
 // Create playlist
-app.post('/users/:user_id/playlists', function (req, res) {
+app.post('/users/:userId/playlists', function (req, res) {
+	Playlist.create({
+		name: 'playlist1',
+		user: req.params.userId,
+		songs: ['Song1', 'Song2']
+	}, function (err, playlist) {
+		if (err) return { error: "Failed to create user!" };
 
+		console.log(playlist);
+	});
 });
 
-app.get('/', function (req, res) {
+app.get('/:test', function (req, res) {
+	console.log(req.params.test);
+
 	res.send('Hello World!');
 });
 
 app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-	console.log('Server listening on port: %s', port);
+	console.log('Server listening on port: %s', PORT);
 });
