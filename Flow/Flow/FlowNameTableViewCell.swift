5e4a313f69b772129d901aa298928133ffc2e0f5
Valentin Perez <valpe70@gmail.com>
2015-11-07 01:00:46 -0500

    moar ios

---
+++ Flow/Flow/FlowNameTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FlowNameTableViewCell.swift
+//  Flow
+//
+//  Created by Valentin Perez on 11/7/15.
+//  Copyright © 2015 Valpe Technologies. All rights reserved.
+//
+
+import UIKit
+
+class FlowNameTableViewCell: UITableViewCell {
+
+  @IBOutlet var nameLabel: UILabel!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
